fa1b236fa5a603168479db0c1f0e2d4af144930d
sergio <sergio@localhost.localdomain>
2025-03-22 16:51:58 +0100

    domain unit tests okkk

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Web system is a .NET solution focused in high performance and good practices.
 ## Annotations
 
 * Crear pipelines
+* Crear test de dependencias entre capas
+* Crear tests de warnings
+* Tests de estrés
---
+++ test/WebSystem.Domain.UnitTests/ValueObjects/ColourTests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+
+using WebSystem.Domain.Exceptions;
+using WebSystem.Domain.ValueObjects;
+
+namespace CleanArchitecture.Domain.UnitTests.ValueObjects;
+
+[TestClass]
+public sealed class ColourTests
+{
+    [TestMethod]
+    public void ShouldReturnCorrectColourCode()
+    {
+        var code = "#FFFFFF";
+
+        var colour = Colour.From(code);
+
+        colour.Code.Should().Be(code);
+    }
+
+    [TestMethod]
+    public void ToStringReturnsCode()
+    {
+        var colour = Colour.White;
+
+        colour.ToString().Should().Be(colour.Code);
+    }
+
+    [TestMethod]
+    public void ShouldPerformImplicitConversionToColourCodeString()
+    {
+        string code = Colour.White;
+
+        code.Should().Be("#FFFFFF");
+    }
+
+    [TestMethod]
+    public void ShouldPerformExplicitConversionGivenSupportedColourCode()
+    {
+        var colour = (Colour)"#FFFFFF";
+
+        colour.Should().Be(Colour.White);
+    }
+
+    [TestMethod]
+    public void ShouldThrowUnsupportedColourExceptionGivenNotSupportedColourCode()
+    {
+        FluentActions.Invoking(() => Colour.From("##FF33CC"))
+            .Should().Throw<UnsupportedColourException>();
+    }
+}
